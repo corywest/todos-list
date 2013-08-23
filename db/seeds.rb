d40b1e8e4a991227c4e66fcb7f58f342253d9cc2
Brian Tong <btong34@gmail.com>
2013-08-23 15:56:07 -0500

    made seed file

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+require_relative '../app/models/task'
+require_relative '../app/models/list'
+
+require 'faker'
+
+module TodoSeed
+  
+  def self.import_tasks
+    Task.transaction do  
+      10.times do
+        Task.create!(
+          :content => Faker::Lorem.sentence(word_count = 4),
+          :list_id => 1
+        )
+    end
+  end
+
+  def self.import_list
+    List.transaction do
+      List.create!
+    end
+  end
+
+end
+
+TodoSeed.import_list
+TodoSeed.import_tasks
